0c43e969c969c9db10b43bc09c3b45eb6e328b0a
ycerdan <ycerdan@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-10 09:33:13 +0000

    Commit to TER version 1.0.9
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/recordsmanager/trunk@60532 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "recordsmanager".
 #
-# Auto generated 07-09-2011 08:32
+# Auto generated 14-12-2011 15:29
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -14,22 +14,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Records management in a BE module',
 	'description' => 'Add modules to easily manage your records (insert, edit & export) in one place.',
 	'category' => 'module',
-	'author' => 'CERDAN Yohann [Site-nGo]',
-	'author_email' => 'cerdanyohann@yahoo.fr',
-	'shy' => '',
+	'shy' => 0,
+	'version' => '1.0.9',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'stable',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
+	'author' => 'CERDAN Yohann [Site-nGo]',
+	'author_email' => 'cerdanyohann@yahoo.fr',
 	'author_company' => '',
-	'version' => '1.0.8',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -40,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:59:{s:9:"ChangeLog";s:4:"2e3f";s:21:"ext_conf_template.txt";s:4:"999f";s:12:"ext_icon.gif";s:4:"914a";s:17:"ext_localconf.php";s:4:"ba27";s:14:"ext_tables.php";s:4:"7a59";s:14:"ext_tables.sql";s:4:"b50a";s:33:"icon_tx_recordsmanager_config.gif";s:4:"914a";s:16:"locallang_db.xml";s:4:"90d3";s:7:"tca.php";s:4:"f2ee";s:14:"doc/manual.sxw";s:4:"2c83";s:43:"hooks/class.tx_recordsmanager_callhooks.php";s:4:"e379";s:40:"lib/class.tx_recordsmanager_flexfill.php";s:4:"ef04";s:14:"lib/module.css";s:4:"b31c";s:34:"lib/php_writeexcel-0.3.0/CHANGELOG";s:4:"3434";s:32:"lib/php_writeexcel-0.3.0/CONTACT";s:4:"c267";s:28:"lib/php_writeexcel-0.3.0/FAQ";s:4:"ec96";s:31:"lib/php_writeexcel-0.3.0/HEADER";s:4:"e6b3";s:33:"lib/php_writeexcel-0.3.0/HOMEPAGE";s:4:"5a02";s:32:"lib/php_writeexcel-0.3.0/LICENSE";s:4:"7fbc";s:31:"lib/php_writeexcel-0.3.0/README";s:4:"4e69";s:31:"lib/php_writeexcel-0.3.0/THANKS";s:4:"5969";s:29:"lib/php_writeexcel-0.3.0/TODO";s:4:"3bb0";s:60:"lib/php_writeexcel-0.3.0/class.writeexcel_biffwriter.inc.php";s:4:"b171";s:56:"lib/php_writeexcel-0.3.0/class.writeexcel_format.inc.php";s:4:"69fd";s:57:"lib/php_writeexcel-0.3.0/class.writeexcel_formula.inc.php";s:4:"74a1";s:59:"lib/php_writeexcel-0.3.0/class.writeexcel_olewriter.inc.php";s:4:"188f";s:58:"lib/php_writeexcel-0.3.0/class.writeexcel_workbook.inc.php";s:4:"f9c5";s:61:"lib/php_writeexcel-0.3.0/class.writeexcel_workbookbig.inc.php";s:4:"a487";s:59:"lib/php_writeexcel-0.3.0/class.writeexcel_worksheet.inc.php";s:4:"afe0";s:44:"lib/php_writeexcel-0.3.0/example-bigfile.php";s:4:"ac45";s:43:"lib/php_writeexcel-0.3.0/example-colors.php";s:4:"b834";s:41:"lib/php_writeexcel-0.3.0/example-demo.php";s:4:"944f";s:43:"lib/php_writeexcel-0.3.0/example-merge2.php";s:4:"2d8d";s:42:"lib/php_writeexcel-0.3.0/example-panes.php";s:4:"a858";s:43:"lib/php_writeexcel-0.3.0/example-repeat.php";s:4:"f748";s:43:"lib/php_writeexcel-0.3.0/example-simple.php";s:4:"a531";s:43:"lib/php_writeexcel-0.3.0/example-stocks.php";s:4:"26d1";s:45:"lib/php_writeexcel-0.3.0/example-textwrap.php";s:4:"33fe";s:61:"lib/php_writeexcel-0.3.0/functions.writeexcel_utility.inc.php";s:4:"ca81";s:32:"lib/php_writeexcel-0.3.0/php.bmp";s:4:"de9a";s:13:"mod1/conf.php";s:4:"8a46";s:14:"mod1/index.php";s:4:"d3f8";s:18:"mod1/locallang.xml";s:4:"0f7b";s:22:"mod1/locallang_mod.xml";s:4:"3fae";s:19:"mod1/moduleicon.gif";s:4:"983a";s:13:"mod2/conf.php";s:4:"194a";s:14:"mod2/index.php";s:4:"a0df";s:18:"mod2/locallang.xml";s:4:"cb07";s:22:"mod2/locallang_mod.xml";s:4:"6575";s:19:"mod2/moduleicon.gif";s:4:"448f";s:13:"mod3/conf.php";s:4:"291b";s:14:"mod3/index.php";s:4:"fb21";s:18:"mod3/locallang.xml";s:4:"2a21";s:22:"mod3/locallang_mod.xml";s:4:"0068";s:19:"mod3/moduleicon.gif";s:4:"7111";s:16:"modmain/conf.php";s:4:"f5eb";s:17:"modmain/index.php";s:4:"e40e";s:25:"modmain/locallang_mod.xml";s:4:"e606";s:22:"modmain/moduleicon.gif";s:4:"914a";}',
+	'_md5_values_when_last_written' => 'a:59:{s:9:"ChangeLog";s:4:"1fa8";s:21:"ext_conf_template.txt";s:4:"999f";s:12:"ext_icon.gif";s:4:"914a";s:17:"ext_localconf.php";s:4:"ba27";s:14:"ext_tables.php";s:4:"7a59";s:14:"ext_tables.sql";s:4:"57c2";s:33:"icon_tx_recordsmanager_config.gif";s:4:"914a";s:16:"locallang_db.xml";s:4:"90d3";s:7:"tca.php";s:4:"f2ee";s:14:"doc/manual.sxw";s:4:"2c83";s:43:"hooks/class.tx_recordsmanager_callhooks.php";s:4:"e379";s:40:"lib/class.tx_recordsmanager_flexfill.php";s:4:"ef04";s:14:"lib/module.css";s:4:"b31c";s:34:"lib/php_writeexcel-0.3.0/CHANGELOG";s:4:"3434";s:32:"lib/php_writeexcel-0.3.0/CONTACT";s:4:"c267";s:28:"lib/php_writeexcel-0.3.0/FAQ";s:4:"ec96";s:31:"lib/php_writeexcel-0.3.0/HEADER";s:4:"e6b3";s:33:"lib/php_writeexcel-0.3.0/HOMEPAGE";s:4:"5a02";s:32:"lib/php_writeexcel-0.3.0/LICENSE";s:4:"7fbc";s:31:"lib/php_writeexcel-0.3.0/README";s:4:"4e69";s:31:"lib/php_writeexcel-0.3.0/THANKS";s:4:"5969";s:29:"lib/php_writeexcel-0.3.0/TODO";s:4:"3bb0";s:60:"lib/php_writeexcel-0.3.0/class.writeexcel_biffwriter.inc.php";s:4:"b171";s:56:"lib/php_writeexcel-0.3.0/class.writeexcel_format.inc.php";s:4:"69fd";s:57:"lib/php_writeexcel-0.3.0/class.writeexcel_formula.inc.php";s:4:"74a1";s:59:"lib/php_writeexcel-0.3.0/class.writeexcel_olewriter.inc.php";s:4:"188f";s:58:"lib/php_writeexcel-0.3.0/class.writeexcel_workbook.inc.php";s:4:"7cff";s:61:"lib/php_writeexcel-0.3.0/class.writeexcel_workbookbig.inc.php";s:4:"a487";s:59:"lib/php_writeexcel-0.3.0/class.writeexcel_worksheet.inc.php";s:4:"afe0";s:44:"lib/php_writeexcel-0.3.0/example-bigfile.php";s:4:"ac45";s:43:"lib/php_writeexcel-0.3.0/example-colors.php";s:4:"b834";s:41:"lib/php_writeexcel-0.3.0/example-demo.php";s:4:"944f";s:43:"lib/php_writeexcel-0.3.0/example-merge2.php";s:4:"2d8d";s:42:"lib/php_writeexcel-0.3.0/example-panes.php";s:4:"a858";s:43:"lib/php_writeexcel-0.3.0/example-repeat.php";s:4:"f748";s:43:"lib/php_writeexcel-0.3.0/example-simple.php";s:4:"a531";s:43:"lib/php_writeexcel-0.3.0/example-stocks.php";s:4:"26d1";s:45:"lib/php_writeexcel-0.3.0/example-textwrap.php";s:4:"33fe";s:61:"lib/php_writeexcel-0.3.0/functions.writeexcel_utility.inc.php";s:4:"ca81";s:32:"lib/php_writeexcel-0.3.0/php.bmp";s:4:"de9a";s:13:"mod1/conf.php";s:4:"8a46";s:14:"mod1/index.php";s:4:"d3f8";s:18:"mod1/locallang.xml";s:4:"0f7b";s:22:"mod1/locallang_mod.xml";s:4:"3fae";s:19:"mod1/moduleicon.gif";s:4:"983a";s:13:"mod2/conf.php";s:4:"194a";s:14:"mod2/index.php";s:4:"a0df";s:18:"mod2/locallang.xml";s:4:"cb07";s:22:"mod2/locallang_mod.xml";s:4:"6575";s:19:"mod2/moduleicon.gif";s:4:"448f";s:13:"mod3/conf.php";s:4:"291b";s:14:"mod3/index.php";s:4:"4d34";s:18:"mod3/locallang.xml";s:4:"2a21";s:22:"mod3/locallang_mod.xml";s:4:"0068";s:19:"mod3/moduleicon.gif";s:4:"7111";s:16:"modmain/conf.php";s:4:"f5eb";s:17:"modmain/index.php";s:4:"e40e";s:25:"modmain/locallang_mod.xml";s:4:"e606";s:22:"modmain/moduleicon.gif";s:4:"914a";}',
 	'suggests' => array(
 	),
 );
